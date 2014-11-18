c280c113a63750851ecc09aa89b196569fd81243
Luc Perkins <lucperkins@gmail.com>
2014-11-18 12:17:55 -0800

    still struggling with TCP

--- src/main/scala/butterfly/Main.scala
+++ src/main/scala/butterfly/Main.scala
@@ -2,11 +2,17 @@ package butterfly
 
 import java.net.InetSocketAddress
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.io.Tcp.{Write, CommandFailed, Bind}
+import akka.io.{Tcp, IO}
+import akka.util.ByteString
+import com.basho.riak.protobuf.RiakKvPB.RpbGetReq
+import nl.gideondk.sentinel.Client
+
+import scala.concurrent.duration.Duration
 
 object Main extends App {
+  val address = new InetSocketAddress("127.0.0.1", 10017)
   val system = ActorSystem("riak-system")
-  val endpoint = new InetSocketAddress("localhost", 10017)
-  system.actorOf(RiakClient.props(endpoint), "riak-connection")
-
+  system.actorOf(RiakConnection.props(address), "riak-actor")
 }
